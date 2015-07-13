5d264602d1e4fcfad4bc04ec2e3fa20f5685a629
Tom VanAntwerp <vanantwerp@gmail.com>
2015-07-13 14:34:36 -0400

    Data through HW, add basic filtering

--- js/app.js
+++ js/app.js
@@ -3,32 +3,45 @@
 
   var app,
     views,
-    DATA_PATH = 'data/location-matters-2015.csv';
+    DATA_PATH = 'data/location-matters-2015.json';
 
   $(function() {
-    d3.csv(DATA_PATH, function(d) {
-      return {
-        name: d.name,
-        value: +d.value
-      };
-    }, app.initialize);
+    d3.json(DATA_PATH, function(error, data) {
+      if (error) { return error; }
+
+      app.initialize(data);
+    });
   });
 
   app = {
     initialize: function(data) {
-      console.log(data);
+      this.data = data;
+      views.stateBars(this.filter());
+    },
+
+    filter: function(value) {
+      var state = value || $('#state-select').val();
+      var stateData = this.data.filter(function(stateData) {
+        return stateData.name === state;
+      });
+
+      return stateData;
     }
   }
 
-  views = {};
+  views = {
+    stateBars: function(data) {
+      var _this = this;
 
-  views.stateBars = function(data) {
-    var _this = this;
+      this.data = data;
+      console.log(this.data);
+      this.element = d3.select('#state-charts');
+      this.svg = this.element.append('svg');
+      this.x = d3.scale.ordinal().rangeRoundBands([0, 600]);
+      this.y = d3.scale.linear().rangeRound([0, 500]);
+      this.xAxis = d3.svg.axis().scale(this.x).orient('bottom');
+      this.yAxis = d3.svg.axis().scale(this.y).orient('left');
 
-    this.data = data;
-    this.element = d3.select('#state-charts');
-    this.svg = element.append('svg');
-    this.x = d3.scale().linear().domain([0, 600]);
-    this.y = d3.scale().linear().domain([0, 500]);
-  }
+    }
+  };
 }());
