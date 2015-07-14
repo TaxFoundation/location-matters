da0d015e1a24841d99bb402474e95cece369491a
Tom VanAntwerp <vanantwerp@gmail.com>
2015-07-14 16:29:16 -0400

    Brute force remove old elements, fixes #1

--- js/app.js
+++ js/app.js
@@ -38,37 +38,39 @@
     initialize: function(data) {
       this.dimensions = {
         width: 600,
-        height: 500
+        height: 500,
+        margin: {
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20
+        }
       };
 
-      this.svg = d3.select('#state-charts').append('svg').attr('height', views.dimensions.height).attr('width', views.dimensions.width);
+      this.svg = d3.select('#state-charts').append('svg')
+        .attr('height', views.dimensions.height + views.dimensions.margin.top + views.dimensions.margin.bottom)
+        .attr('width', views.dimensions.width + views.dimensions.margin.right + views.dimensions.margin.left);
       this.x = d3.scale.ordinal().rangeRoundBands([0, views.dimensions.width], 0.1);
       this.y = d3.scale.linear().rangeRound([0, views.dimensions.height]).domain([-0.3, 0.3]);
       this.xAxis = d3.svg.axis().scale(views.x).orient('bottom');
       this.yAxis = d3.svg.axis().scale(views.y).orient('left');
 
-      views.svg.append('g')
-        .attr('class', 'x axis')
-        .attr('transform', 'translate(0,' + 500 + ')')
-        .call(views.xAxis);
-
-      views.svg.append('g')
-          .attr('class', 'y axis')
-          .call(views.yAxis)
-        .append('text')
-          .attr('y', 6)
-          .attr('dy', '.71em')
-          .style('text-anchor', 'end');
-
       views.draw(data);
     },
 
     draw: function(data) {
+      d3.selectAll('g').remove();
       var bars = views.svg.selectAll('.firm').data(data.firms);
       views.x.domain(data.firms.map(function(d) { return d.name; }));
 
-      bars.exit()
-        .remove();
+      views.svg.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0,' + views.dimensions.height + ')')
+        .call(views.xAxis);
+
+      views.svg.append('g')
+          .attr('class', 'y axis')
+          .call(views.yAxis);
 
       bars.enter()
         .append('g')
