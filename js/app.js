2085aeb0b4e69c8a96ff042f9377e7d48b80f937
Tom VanAntwerp <vanantwerp@gmail.com>
2015-07-14 16:15:28 -0400

    Draw dual bar chart

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@
       views.initialize(app.filter());
 
       $('#state-select').change(function(e) { // Watch for changes to the state selection dropdown and call app.filter
-        views.initialize(app.filter($(e.target).attr('value')));
+        views.draw(app.filter($(e.target).attr('value')));
       });
     },
 
@@ -36,40 +36,58 @@
 
   views = {
     initialize: function(data) {
-      this.data = data;
       this.dimensions = {
         width: 600,
         height: 500
       };
 
-      var svg = d3.select('#state-charts').append('svg'),
-        x = d3.scale.ordinal().rangeRoundBands([0, views.dimensions.width], 0.1).domain(views.data.firms.map(function(d) { return d.name; })),
-        y = d3.scale.linear().rangeRound([0, views.dimensions.height]),
-        xAxis = d3.svg.axis().scale(x).orient('bottom'),
-        yAxis = d3.svg.axis().scale(y).orient('left');
+      this.svg = d3.select('#state-charts').append('svg').attr('height', views.dimensions.height).attr('width', views.dimensions.width);
+      this.x = d3.scale.ordinal().rangeRoundBands([0, views.dimensions.width], 0.1);
+      this.y = d3.scale.linear().rangeRound([0, views.dimensions.height]).domain([-0.3, 0.3]);
+      this.xAxis = d3.svg.axis().scale(views.x).orient('bottom');
+      this.yAxis = d3.svg.axis().scale(views.y).orient('left');
 
-      svg.append('g')
+      views.svg.append('g')
         .attr('class', 'x axis')
         .attr('transform', 'translate(0,' + 500 + ')')
-        .call(xAxis);
+        .call(views.xAxis);
 
-      svg.append('g')
+      views.svg.append('g')
           .attr('class', 'y axis')
-          .call(yAxis)
+          .call(views.yAxis)
         .append('text')
           .attr('y', 6)
           .attr('dy', '.71em')
           .style('text-anchor', 'end');
 
-      var firmGroups = svg.selectAll('.state')
-          .data(views.data.firms)
-        .enter().append('g')
-          .attr('transform', function(d) { return 'translate(' + x(d.name) + ',0)'; });
+      views.draw(data);
+    },
 
-      firmGroups.append('rect')
-        .attr('y', function(d) { return y(d.new['Income Tax']); })
-        .attr('height', function(d) { return views.dimensions.height - y(d.new['Income Tax']); })
-        .attr('width', 10)
+    draw: function(data) {
+      var bars = views.svg.selectAll('.firm').data(data.firms);
+      views.x.domain(data.firms.map(function(d) { return d.name; }));
+
+      bars.exit()
+        .remove();
+
+      bars.enter()
+        .append('g')
+        .attr('class', 'firm');
+
+      bars
+        .append('rect')
+        .attr('y', function(d) { return views.y(+d.old['Income Tax']); })
+        .attr('height', function(d) { return views.dimensions.height - views.y(+d.old['Income Tax']); })
+        .attr('width', views.x.rangeBand() / 2)
+        .attr('x', function(d) { return views.x(d.name); })
+        .attr('fill', '#0094ff');
+
+      bars
+        .append('rect')
+        .attr('y', function(d) { return views.y(+d.new['Income Tax']); })
+        .attr('height', function(d) { return views.dimensions.height - views.y(+d.new['Income Tax']); })
+        .attr('width', views.x.rangeBand() / 2)
+        .attr('x', function(d) { return views.x(d.name) + views.x.rangeBand() / 2; })
         .attr('fill', '#0094ff');
     }
   };
